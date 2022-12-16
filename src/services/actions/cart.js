d18116817183b71358c20addd05290100ed2aef6
Alexey Gamov <lexa2009xxxx@yandex.ru>
2022-12-16 15:13:52 +0300

    feat: add action fakeApi (getItems)

--- src/services/actions/cart.js
+++ src/services/actions/cart.js
@@ -1,6 +1,36 @@
+import { getItemsRequest } from "../fakeApi";
+
 const INCREASE_ITEM = "INCREASE_ITEM";
 const DECREASE_ITEM = "DECREASE_ITEM";
 const DELETE_ITEM = "DELETE_ITEM";
 const CANCEL_PROMO = "CANCEL_PROMO";
 const TAB_SWITCH = "TAB_SWITCH";
-export { INCREASE_ITEM, DECREASE_ITEM, DELETE_ITEM, CANCEL_PROMO, TAB_SWITCH };
+
+const GET_ITEMS_REQUEST = "GET_ITEMS_REQUEST";
+const GET_ITEMS_SUCCESS = "GET_ITEMS_SUCCESS";
+const GET_ITEMS_FAILED = "GET_ITEMS_FAILED";
+
+function getItems() {
+  return function (dispatch) {
+    dispatch({ type: GET_ITEMS_REQUEST });
+    getItemsRequest()
+      .then((res) => {
+        if (res && res.success) {
+          dispatch({ type: GET_ITEMS_SUCCESS, items: res.data });
+        } else dispatch({ type: GET_ITEMS_FAILED });
+      })
+      .catch((err) => dispatch({ type: GET_ITEMS_FAILED }));
+  };
+}
+
+export {
+  INCREASE_ITEM,
+  DECREASE_ITEM,
+  DELETE_ITEM,
+  CANCEL_PROMO,
+  TAB_SWITCH,
+  getItems,
+  GET_ITEMS_REQUEST,
+  GET_ITEMS_SUCCESS,
+  GET_ITEMS_FAILED,
+};
--- src/services/reducers/cart.js
+++ src/services/reducers/cart.js
@@ -1,54 +1,84 @@
 import {
-  INCREASE_ITEM,
-  DECREASE_ITEM,
   DELETE_ITEM,
   CANCEL_PROMO,
+  DECREASE_ITEM,
+  INCREASE_ITEM,
+  GET_ITEMS_FAILED,
+  GET_ITEMS_REQUEST,
+  GET_ITEMS_SUCCESS,
   TAB_SWITCH,
 } from "../actions/cart";
-
-import { recommendedItems, items } from "../initialData";
+import { recommendedItems } from "../initialData";
 
 const initialState = {
-  items: items,
-  recommendedItems: recommendedItems,
-  promoCode: "",
-  promoDiscount: null,
+  items: [],
+  itemsRequest: false,
+  itemsFailed: false,
+
+  recommendedItems,
+
+  promoCode: "PROMOCODE",
+  promoDiscount: 50,
+
   currentTab: "items",
 };
 
 export const cartReducer = (state = initialState, action) => {
   switch (action.type) {
-    case INCREASE_ITEM:
+    case GET_ITEMS_REQUEST: {
+      return {
+        ...state,
+        itemsRequest: true,
+      };
+    }
+    case GET_ITEMS_SUCCESS: {
+      return {
+        ...state,
+        itemsFailed: false,
+        items: action.items,
+        itemsRequest: false,
+      };
+    }
+    case GET_ITEMS_FAILED: {
+      return { ...state, itemsFailed: true, itemsRequest: false };
+    }
+    case TAB_SWITCH: {
+      return {
+        ...state,
+        currentTab: state.currentTab === "items" ? "postponed" : "items",
+      };
+    }
+    case INCREASE_ITEM: {
       return {
         ...state,
         items: [...state.items].map((item) =>
-          item.id === action.id ? { ...item, qty: item.qty + 1 } : item
+          item.id === action.id ? { ...item, qty: ++item.qty } : item
         ),
       };
-    case DECREASE_ITEM:
+    }
+    case DECREASE_ITEM: {
       return {
         ...state,
         items: [...state.items].map((item) =>
-          item.id === action.id ? { ...item, qty: item.qty - 1 } : item
+          item.id === action.id ? { ...item, qty: --item.qty } : item
         ),
       };
-    case DELETE_ITEM:
+    }
+    case DELETE_ITEM: {
       return {
         ...state,
         items: [...state.items].filter((item) => item.id !== action.id),
       };
-    case CANCEL_PROMO:
+    }
+    case CANCEL_PROMO: {
       return {
         ...state,
         promoCode: "",
         promoDiscount: null,
       };
-    case TAB_SWITCH:
-      return {
-        ...state,
-        currentTab: state.currentTab === "items" ? "postponed" : "items",
-      };
-    default:
+    }
+    default: {
       return state;
+    }
   }
 };
